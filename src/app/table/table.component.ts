725a7727374375a220ed1d74977e82eaabaa0320
DuskoPeric <Dusko.Peric@homegate.ch>
2019-06-12 16:16:46 +0200

    chanage sorting logic

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -30,6 +30,43 @@ export class TableComponent implements OnInit {
           this.playerList.push(element);
         })
         this.playerList.sort((a, b) => (a.score < b.score) ? 1 : -1);
+
+        var round = this.generalService.getGroupList();
+
+        round.snapshotChanges().subscribe(item => {
+          var tmpArr=[];
+          item.forEach(element => {
+            var y = element.payload.toJSON();
+            y["id"]=element.key;
+            tmpArr.push(y);
+          });
+          var arrArr=[];
+          var convertArr=Object.values(tmpArr[0])
+
+          for (let m = 0; m < convertArr.length; m++) {
+            arrArr.push(Object.values(convertArr[m]))
+          }
+
+          var prewScore=0;
+          for (let i = 0; i < this.playerList.length; i++) {
+            if(prewScore==this.playerList[i].score){
+              
+              for (let j = 0; j < arrArr.length; j++) {
+                for (let k = 0; k < arrArr[j].length; k++) {
+                  if(this.playerList[i].name==arrArr[j][k].p1 && this.playerList[i-1].name==arrArr[j][k].p2 && arrArr[j][k].winner=="p1" || this.playerList[i].name==arrArr[j][k].p2 && this.playerList[i-1].name==arrArr[j][k].p1 && arrArr[j][k].winner=="p2"){
+                    var tmp=this.playerList[i];
+                    this.playerList[i]=this.playerList[i-1];
+                    this.playerList[i-1]=tmp;
+                        }
+                  
+                }
+                
+              }
+            }
+            prewScore=this.playerList[i].score;
+          }
+        });
+
         this.players=this.playerList;
         this.loading=false;
       }
